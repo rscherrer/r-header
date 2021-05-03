c20106002a75d85beb0fa3b88f29b0e4474bbaf4
rscherrer <raphael.scherrer@evobio.eu>
2021-05-03 12:52:54 +0200

    done

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
+# r-header
 
+Here is the header I like to add to my standalone R scripts (i.e. not scripts that contain functions as part of a package).
+
+```{r}
+## ---------------------------
+##
+## Script name: 
+##
+## Purpose of script:
+##
+## How to use:
+##
+## Author: Raphael Scherrer
+##
+## Date Created: `r paste(Sys.Date())`
+##
+## This script comes with no guarantee whatsoever.
+##
+## Copyright (c) Raphael Scherrer, `r paste(format(Sys.Date(), "%Y"))`
+##
+## Find me on GitHub at https://github.com/rscherrer
+##
+## Email: 
+## r.scherrer@rug.nl
+## raphael.scherrer@evobio.eu
+## raph.rjfs@hotmail.fr
+##
+## ---------------------------
+``` 
+
+A header can be automatically added to a script in RStudio by using [snippets](https://support.rstudio.com/hc/en-us/articles/204463668-Code-Snippets).
+
+This was inspired by stumbling upon Tim Farewell's [website](https://timfarewell.co.uk/my-r-script-header-template/).
---
+++ header.R
@@ -0,0 +1,39 @@
+## ---------------------------
+##
+## Script name: header.R
+##
+## Purpose of script:
+##
+## Show an example.
+##
+## How to use:
+##
+## (1) This script is made to work on data X
+##
+## (2) You must set up the `input` and `output` variables as paths to where
+## the data is to be found, and where the results are to be saved, respectively
+##
+## (3) The `input` folder is assumed to have the following folder structure...
+##
+## (4) This script was run and worked fine on <date> using packages Y v1.0,
+## Z v1.3 and W v2.0
+##
+## Author: Raphael Scherrer
+##
+## Date Created: 2021-05-03
+##
+## This script comes with no guarantee whatsoever.
+##
+## Copyright (c) Raphael Scherrer, 2021
+##
+## Find me on GitHub at https://github.com/rscherrer
+##
+## Email:
+## r.scherrer@rug.nl
+## raphael.scherrer@evobio.eu
+## raph.rjfs@hotmail.fr
+##
+## ---------------------------
+
+rm(list = ls())
+print("Hello world!")
